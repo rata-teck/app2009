00f3a541bb72b8adbb9f3af273a222a488b4413e
rata-teck <shadowsmash5@gmail.com>
2022-09-20 16:50:01 -0300

    base2

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent} from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    RouterModule
   ]
 })
 export class AuthModule { }
